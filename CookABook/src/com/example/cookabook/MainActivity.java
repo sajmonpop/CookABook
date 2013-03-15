7b24eb9ab134fa5837fe09263fe7858eeaa087b4
Simon Andersson <s.andersson87@hotmail.com>
2013-03-15 16:27:22 +0100

    v1
    v1

---
+++ CookABook/src/com/example/cookabook/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.cookabook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.devspark.sidenavigation.ISideNavigationCallback;
+import com.devspark.sidenavigation.SideNavigationView;
+import com.devspark.sidenavigation.SideNavigationView.Mode;
+
+public class MainActivity extends SherlockActivity implements ISideNavigationCallback {
+
+    public static final String EXTRA_TITLE = "com.devspark.sidenavigation.sample.extra.MTGOBJECT";
+    public static final String EXTRA_RESOURCE_ID = "com.devspark.sidenavigation.sample.extra.RESOURCE_ID";
+    public static final String EXTRA_MODE = "com.devspark.sidenavigation.sample.extra.MODE";
+    private SideNavigationView sideNavigationView;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		sideNavigationView = (SideNavigationView) findViewById(R.id.side_navigation_view);
+        sideNavigationView.setMenuItems(R.menu.side_navigation_menu);
+        sideNavigationView.setMenuClickCallback(this);
+
+        if (getIntent().hasExtra(EXTRA_TITLE)) {
+            String title = getIntent().getStringExtra(EXTRA_TITLE);
+            int resId = getIntent().getIntExtra(EXTRA_RESOURCE_ID, 0);
+            setTitle(title);
+            
+            sideNavigationView.setMode(getIntent().getIntExtra(EXTRA_MODE, 0) == 0 ? Mode.LEFT : Mode.RIGHT);
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+        if (sideNavigationView.getMode() == Mode.RIGHT) {
+            menu.findItem(R.id.mode_right).setChecked(true);
+        } else {
+            menu.findItem(R.id.mode_left).setChecked(true);
+        }
+        return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                sideNavigationView.toggleMenu();
+                break;
+            case R.id.mode_left:
+                item.setChecked(true);
+                sideNavigationView.setMode(Mode.LEFT);
+                break;
+            case R.id.mode_right:
+                item.setChecked(true);
+                sideNavigationView.setMode(Mode.RIGHT);
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+	@Override
+	public void onSideNavigationItemClick(int itemId) {
+		// TODO Auto-generated method stub
+		
+	} 
+
+    @Override
+    public void onBackPressed() {
+        // hide menu if it shown
+        if (sideNavigationView.isShown()) {
+            sideNavigationView.hideMenu();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * Start activity from SideNavigation.
+     * 
+     * @param title title of Activity
+     * @param resId resource if of background image
+     */
+    private void invokeActivity(String title, int resId) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(EXTRA_TITLE, title);
+        intent.putExtra(EXTRA_RESOURCE_ID, resId);
+        intent.putExtra(EXTRA_MODE, sideNavigationView.getMode() == Mode.LEFT ? 0 : 1);
+
+        // all of the other activities on top of it will be closed and this
+        // Intent will be delivered to the (now on top) old activity as a
+        // new Intent.
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(intent);
+        // no animation of transition
+        overridePendingTransition(0, 0);
+    }
+
+}
